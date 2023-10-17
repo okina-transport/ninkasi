366dcbd94fc7f7f5c779a658bd8740368feed1d1
gcarrere <gcarrere@okina.fr>
2023-10-17 16:12:23 +0200

    18583 _ new role : EXPLOITANT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill';
 import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
 import Keycloak from 'keycloak-js';
 import App from './containers/App';
 import configureStore from './store/store';
@@ -10,42 +10,70 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import cfgreader from './config/readConfig';
 
 
+
 cfgreader.readConfig(
-  function(config) {
-    window.config = config;
-    authWithKeyCloak(config.endpointBase);
-  }.bind(this)
+    function (config) {
+        window.config = config;
+        authWithKeyCloak(config.endpointBase);
+    }.bind(this)
 );
 
 function authWithKeyCloak(endpointBase) {
-  let kc = new Keycloak(endpointBase + 'config/keycloak.json');
+    let kc = new Keycloak(endpointBase + 'config/keycloak.json');
 
-  kc
-    .init({ onLoad: 'login-required', checkLoginIframe: false })
-    .success(authenticated => {
-      if (authenticated) {
-        localStorage.setItem('NINKASI::jwt', kc.token);
+    kc
+        .init({onLoad: 'login-required', checkLoginIframe: false})
+        .success(authenticated => {
+            if (authenticated) {
+                localStorage.setItem('NINKASI::jwt', kc.token);
 
-        setInterval(() => {
-          kc.updateToken(10).error(() => kc.logout());
-          localStorage.setItem('NINKASI::jwt', kc.token);
-        }, 10000);
 
-        renderIndex(kc);
-      } else {
-        kc.login();
-      }
-    });
+                setInterval(() => {
+                    kc.updateToken(10).error(() => kc.logout());
+                    localStorage.setItem('NINKASI::jwt', kc.token);
+                }, 10000);
+
+                   if (canAccessNinkasi(kc)){
+                       renderIndex(kc);
+                   }else{
+                       renderUnauthorizedMessage();
+                   }
+            } else {
+                kc.login();
+            }
+        });
+}
+
+function canAccessNinkasi(kc) {
+    for (let i = 0; i < kc.tokenParsed.realm_access.roles.length; i++) {
+        let role =  kc.tokenParsed.realm_access.roles[i];
+        if (role === "canAccessNinkasi"){
+            return true;
+        }
+    }
+    return false;
+}
+
+function renderUnauthorizedMessage() {
+
+    render(
+        <div>
+            <div>
+                <p>Unauthorized</p>
+            </div>
+        </div>,
+        document.getElementById('root')
+    );
 }
 
 function renderIndex(kc) {
-  const store = configureStore(kc);
-  injectTapEventPlugin();
+    const store = configureStore(kc);
+    injectTapEventPlugin();
 
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('root')
-  );
+    render(
+        <Provider store={store}>
+            <App/>
+        </Provider>,
+        document.getElementById('root')
+    );
 }
