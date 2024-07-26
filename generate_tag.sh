d274d7c5dbc11946cda3171cccdde957546e5690
afreche <alexandre.freche@lumiplan.com>
2024-07-26 14:12:24 +0200

    Update generate_tag.sh script
    - add set -e to stop on any command error
    - add set -x to debug
    - refactoring

--- generate_tag.sh
+++ generate_tag.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
-
 set -ex
-
 if [ $# -eq 1 ]; then
   UPDATE_TYPE=$1
 fi
-
 TAG_NAME=$(npm version "$UPDATE_TYPE" -m "New develop version: %s")
-
-git push origin tag "$TAG_NAME"
 git push
-
+git push origin tag "$TAG_NAME"
 exit 0
